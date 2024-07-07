391ed2fc3f145a2ba0652446079df63dd0665e16
Anner Ronaldo Escobar cruz <anner123escobar@gmail.com>
2024-07-06 20:53:36 -0600

    componentes creados

---
+++ src/app/add-case/add-case.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-case',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-case.component.html',
+  styleUrl: './add-case.component.css'
+})
+export class AddCaseComponent {
+
+}
