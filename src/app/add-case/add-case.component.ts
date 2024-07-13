fb3b1139fd1ab09989bd9d741236b087bc98725c
Anner Ronaldo Escobar cruz <anner123escobar@gmail.com>
2024-07-12 23:14:35 -0600

    se agrego angular material

--- src/app/add-case/add-case.component.ts
+++ src/app/add-case/add-case.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Console } from 'console';
+
 
 @Component({
   selector: 'app-add-case',
@@ -10,28 +10,60 @@ import { Console } from 'console';
   templateUrl: './add-case.component.html',
   styleUrl: './add-case.component.css'
 })
-export class AddCaseComponent {
+export class AddCaseComponent implements OnInit {
+
 
   public myForm: FormGroup = this.fb.group({
-    caseMP:[' ', [Validators.required]],
-    caseDEIC:[' ', [Validators.required]],
-    alertaNumber:[' ', [Validators.required]],
+    tipoCaso:['1', Validators.required],
+    caseMP: ['', [Validators.required]],
+    caseDEIC: ['', [Validators.required]],
+    alertaNumber: ['', [Validators.required]],
 
-    name:[' ', [Validators.required]],
-    edad:['', [Validators.required]],
-    lugar:['',[Validators.required]],
-    gps:['', [Validators.required]],
-
-    investigador:['', [Validators.required]]
+    name: ['', [Validators.required]],
+    edad: ['', [Validators.required]],
+    lugar: ['', [Validators.required]],
+    gps: ['', [Validators.required]],
+    estadoCaso:['1', [Validators.required]],
+    investigador: ['', [Validators.required]]
 
   })
 
+
   constructor(private fb: FormBuilder) { }
 
-  onSave():void{
-  if(this.myForm.invalid) return;
-    console.log(this.myForm.value);
+  ngOnInit(): void {
 
+  }
+
+  isValidField(field: string): boolean | null {
+
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+
+  }
+
+  getFieldError(field: string): string | null {
+
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return 'Minimo tres caracteres';
+      }
+    }
+
+    return null;
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
     this.myForm.reset();
   }
 
