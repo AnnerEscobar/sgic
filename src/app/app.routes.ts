eea492a12853e928e752fbc1c7fae04cc3d28d6f
Anner Escobar <70228927+AnnerEscobar@users.noreply.github.com>
2024-08-19 19:46:11 -0600

    actualizando cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
+import { LoginComponent } from './auth/login/login.component';
 import { Routes } from '@angular/router';
 import { AddCaseComponent } from './add-case/add-case.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   { path: 'dashboard', component: DashboardComponent },
   { path: 'agregar-caso', component: AddCaseComponent },
   { path: 'estadisticas', component: EstadisticasComponent },
